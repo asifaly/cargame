900559168e79df6c1ee671251cff1725c521d425
asifaly <asifaly82@yahoo.com>
2016-03-23 20:11:23 +0530

    border increases with time

--- app.js
+++ app.js
@@ -6,42 +6,112 @@ $(document).ready(function() {
   };
 
   var game = {
-    init: function(sprite) {
+    init: function(sprite, container) {
       this.sprite = sprite;
+      this.container = container;
+      this.isStarted = false;
+
+      //initialize border, left and top values
       $(this.sprite).css('left', '0px');
       $(this.sprite).css('top', '0px');
+      $(this.container).css('border-width', '5px');
 
+      //store the initial values
+      this.dimensions.border = parseInt($(this.container).css('border-width'));
+      this.dimensions.areaWidth = parseInt($(this.container).css('width'));
+      this.dimensions.spriteWidth = parseInt($(this.sprite).css('width'));
+
+      //set initial playArea
+      this.setPlayArea();
+
+      // console.log(this.dimensions);
+    },
+
+    start: function() {
+      this.isStarted = true;
+      this.borderChange();
     },
 
     move: function(side, rev) {
 
-      this.stop();
+      if (this.isStarted) {
 
-      this.intervalId = setInterval(frame, 5);
-      var pos = parseInt($(this.sprite).css(direction[side]));
-      var sprite = this.sprite;
+        //to make reference to the game object in frame function;
+        var that = this;
+        //to clear the interval and restart on direction change
+        this.stop();
+        //move the sprite by calling frame function
+        this.intervalId = setInterval(frame, 5);
+        //get the position of top/left depending on value in side
+        var pos = parseInt($(this.sprite).css(direction[side]));
 
-      function frame() {
-        rev ? pos-- : pos++;
-        pos > 475 || pos < 0 ? game.stop() : $(sprite).css(direction[side], pos + 'px');
-        console.log(pos);
+        function frame() {
+          //if move Up or towards right then decrement else increment pos
+          rev ? pos-- : pos++;
+          //check if pos is left/top is greater than total playarea or less than zero
+          //if yes then game over clear the interval id's
+          //else continue
+          pos > that.dimensions.playArea || pos < 0 ? that.over() : $(that.sprite).css(direction[side], pos + 'px');
+        }
+
       }
 
     },
 
     stop: function() {
+      //stop the timer for movement
       if (this.intervalId) {
         clearInterval(this.intervalId);
       }
     },
 
+    over: function() {
+      //stop the timer for movement and borderchange
+      if (this.borderIntervalId) {
+        clearInterval(this.borderIntervalId);
+      }
+      if (this.intervalId) {
+        clearInterval(this.intervalId);
+      }
+
+      alert("game over");
+      //reset the game
+      game.init(car, gamearea);
+
+    },
+
+    //measure the dimensions and set the max PlayArea
+    setPlayArea: function() {
+      this.dimensions.playArea = this.dimensions.areaWidth - this.dimensions.spriteWidth - (this.dimensions.border * 2);
+    },
+
+    //function to increase borderwidth every 5 secs so play area becomes smaller
+    borderChange: function() {
+      this.dimensions.border = parseInt($(this.container).css('border-width'));
+      this.borderIntervalId = setInterval(border, 5000);
+
+      function border() {
+        game.dimensions.border += 10;
+        game.setPlayArea();
+        $(game.container).css('border-width', game.dimensions.border + 'px');
+      }
+    },
+
+    //store the values of dimensions
+    dimensions: {
+      border: 0,
+      areaWidth: 0,
+      spriteWidth: 0,
+      playArea: 0,
+    }
   };
 
   var car = $('#sprite');
-  game.init(car);
+  var gamearea = $("#container");
+  game.init(car, gamearea);
 
   $('#start').click(function() {
-    game.move(1);
+    game.start();
   });
 
   $('#right').click(function() {
@@ -64,6 +134,8 @@ $(document).ready(function() {
     game.stop();
   });
 
+
+  //handle keypress
   $(document).keydown(function(e) {
     switch (e.which) {
       case 37: // left
